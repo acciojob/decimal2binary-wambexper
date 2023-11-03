320e7ac29abd3f705f5f96d62dc561c2e30be44b
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-03 16:03:53 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  // Check if the input is 0
+  if (num === 0) {
+    return '0';
+  }
+
+  // Initialize an empty string to store the binary representation
+  let binary = '';
+
+  // Convert the decimal number to binary
+  while (num > 0) {
+    // Append the remainder of the division by 2 to the binary string
+    binary = (num % 2) + binary;
+    // Integer division by 2
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
 }
 
-window.decimalToBinary = decimalToBinary;
+const num = parseInt(prompt("Enter a number."));
+alert(decimalToBinary(num));
